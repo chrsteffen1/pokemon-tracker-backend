e86b4f6f493a8a390ce94984f4e6d267b054dff7
Chrsteffen <Chrsteffen1@gmail.com>
2023-02-15 00:34:19 -0500

    get search feild to return proper game name

--- controllers/cartridges.js
+++ controllers/cartridges.js
@@ -3,10 +3,15 @@ import { Cartridge } from "../models/cartridge.js"
 import axios from "axios"
 
 function search(req, res){
-  console.log(req.params.gameId, 'params')
-  axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
+  console.log(req.query.gameQuery, 'params')
+  const gameId = req.query.gameQuery
+  axios.get(`https://pokeapi.co/api/v2/version/${gameId}`)
   .then(response => {
     res.json(response.data) 
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).send("Error retrieving Pokemon")
   })
   
 }
