662039d09da25ea21d57a0540b93c2f53a64ced1
Chrsteffen <Chrsteffen1@gmail.com>
2023-02-14 23:38:40 -0500

    get pokemon api request working

--- controllers/cartridges.js
+++ controllers/cartridges.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 function search(req, res){
   console.log(req.params.gameId, 'params')
-  axios.get(`http://www.boredapi.com/api/activity/`)
+  axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
   .then(response => {
     res.json(response.data) 
   })
